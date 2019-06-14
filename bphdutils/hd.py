e059c16c821017f0460df0e2592e6e43ffec5562
Michael Druk <michael.druk@bitpanda.com>
2019-06-14 11:27:03 +0200

    pass compressed value

--- bphdutils/hd.py
+++ bphdutils/hd.py
@@ -16,7 +16,7 @@ class XPrv(object):
 
     def derive(self, path):
         setup_btcpy('mainnet')
-        return self.__class__(self.key.derive(path).encode())
+        return self.__class__(self.key.derive(path).encode(), compressed=self.compressed)
 
     def prv(self):
         setup_btcpy('mainnet')
@@ -42,7 +42,7 @@ class XPub(object):
 
     def derive(self, path):
         setup_btcpy('mainnet')
-        return self.__class__(self.key.derive(path).encode())
+        return self.__class__(self.key.derive(path).encode(), compressed=self.compressed)
 
     def pub(self):
         setup_btcpy('mainnet')
